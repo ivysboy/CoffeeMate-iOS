2512d564feaccb9d26d9132c29bf2027ca89c19b
徐悟源 <359696929@qq.com>
2017-07-28 00:18:43 +0800

    use web view to display content

--- CoffeeMate/Modules/Home/View/CMArticleContentViewController.m
+++ CoffeeMate/Modules/Home/View/CMArticleContentViewController.m
@@ -11,13 +11,14 @@
 #import "CMHomeArticle.h"
 #import <AFNetworking/UIImageView+AFNetworking.h>
 
-@interface CMArticleContentViewController ()<UIScrollViewDelegate>
+@interface CMArticleContentViewController ()<UIScrollViewDelegate, UIWebViewDelegate>
 
 @property (nonatomic , strong) UIScrollView *scrollView;
 @property (nonatomic , strong) UIImageView *mainImage;
-@property (nonatomic , strong) UILabel *content;
 @property (nonatomic , strong) UILabel *titleLabel;
 @property (nonatomic , strong) CMHomeDataManager *dataManager;
+@property (nonatomic , strong) UIWebView *contentView;
+@property (nonatomic , strong) NSLayoutConstraint *heightContraints;
 
 @end
 
@@ -56,12 +57,11 @@
     [mainImage setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.scrollView addSubview:_mainImage];
     
-    UILabel *content = [[UILabel alloc] initWithFrame:CGRectZero];
-    _content = content;
-    [content setTranslatesAutoresizingMaskIntoConstraints:NO];
-    _content.lineBreakMode = NSLineBreakByWordWrapping;
-    _content.numberOfLines = 0;
-    [self.scrollView addSubview:_content];
+    _contentView = [[UIWebView alloc] initWithFrame:CGRectZero];
+    [_contentView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    _contentView.delegate = self;
+    _contentView.scrollView.scrollEnabled = NO;
+    [self.scrollView addSubview:_contentView];
     
     _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
     [_titleLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
@@ -71,18 +71,20 @@
     _titleLabel.textAlignment = NSTextAlignmentCenter;
     [self.scrollView addSubview:_titleLabel];
     
-    NSDictionary *views = @{@"mainImage" : _mainImage, @"content" : _content, @"title" : _titleLabel};
+    NSDictionary *views = @{@"mainImage" : _mainImage, @"content" : _contentView, @"title" : _titleLabel};
     NSDictionary *metrics = @{@"width" : @(ScreenSize.width)};
     
     
     [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[mainImage]|" options:0 metrics:metrics views:views]];
     [self.scrollView addConstraint:[NSLayoutConstraint constraintWithItem:self.scrollView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.mainImage attribute:NSLayoutAttributeWidth multiplier:1 constant:0]];
     [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[content]|" options:0 metrics:metrics views:views]];
-    [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[mainImage(240)]-60-[content]|" options:0 metrics:nil views:views]];
+    [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[mainImage(240)]-60-[content]-40-|" options:0 metrics:nil views:views]];
     [self.scrollView setContentSize:CGSizeMake(ScreenSize.width, ScreenSize.height)];
     [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[title]-30-|" options:0 metrics:nil views:views]];
     [self.scrollView addConstraint:[NSLayoutConstraint constraintWithItem:self.mainImage attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.titleLabel attribute:NSLayoutAttributeBottom multiplier:1 constant:-20]];
     [self.scrollView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title(40)]" options:0 metrics:nil views:views]];
+    _heightContraints = [NSLayoutConstraint constraintWithItem:_contentView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeHeight multiplier:1.0 constant:30];
+    [self.scrollView addConstraint:_heightContraints];
     
 }
 
@@ -91,9 +93,9 @@
     NSDictionary *param = @{@"articleId" : _articleId};
     [self.dataManager fetchArticleWithParameter:param success:^(CMHomeArticle *data) {
         [_mainImage setImageWithURL:data.image];
-        _content.text = data.content;
         self.title = data.name;
         _titleLabel.text = data.brief;
+        [self loadContent:data.content];
     } failure:^(NSError *error) {
         
     }];
@@ -104,18 +106,28 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
 
+}
+
+
+- (void)loadContent:(NSString *)content {
+    [_contentView loadHTMLString:[self basicHTMLBody:content] baseURL:[[NSBundle mainBundle] bundleURL]];
+}
+
+- (NSString *)basicHTMLBody:(NSString *)content {
+    
+    return [NSString stringWithFormat:@"<html><head><style>%@</style></head><body><div id='box_web_content'>%@</div></body></html>" , [self css],content];
+    
+}
+
+- (NSString *)css {
+    return @"* {font-family: Helvetica;word-break: break-all;}   p {color: #646464;} p.listitem{ padding-left:10px;}p.listitem:before { position:absolute;left:2px;}";
+}
+
+#pragma mark --- WebViewDelegate
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    _heightContraints.constant = webView.scrollView.contentSize.height + 20.0;
 }
 
 @end
